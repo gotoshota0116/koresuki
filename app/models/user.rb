ecd80dd98858702e7810b2eadf100abcfe039ecd
gotoshota0116 <lgy.w30@gmail.com>
2025-01-29 19:22:25 +0900

    style:rubocop,erb-lint修正実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-         
-         validates :name, presence: true, length: { maximum: 30 }
+
+  validates :name, presence: true, length: { maximum: 30 }
 end

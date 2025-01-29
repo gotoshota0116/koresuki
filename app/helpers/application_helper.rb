1d1e46124182c0bc1c7bde4ce5122eda61bbf668
gotoshota0116 <lgy.w30@gmail.com>
2025-01-29 15:25:31 +0900

    add:フラッシュメッセージにtailwind反映のため、ヘルパー定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+	def flash_class(key)
+		case key.to_sym # key が文字列でも対応できるように to_sym で変換
+		when :notice then "bg-green-500 text-white p-4 rounded-md"
+		when :alert  then "bg-red-500 text-white p-4 rounded-md"
+		else "bg-blue-500 text-white p-4 rounded-md"
+	  end
+	end
+  end
+  

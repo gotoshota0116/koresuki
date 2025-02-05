3e81682fe26f12a0e555552b9a70e23fd6309899
gotoshota0116 <lgy.w30@gmail.com>
2025-02-06 02:11:42 +0900

    gem-fakerインスト、seedファイル編集,実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many :boards, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
 

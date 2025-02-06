b2a8fa82c79a1efea2b0958b132eb9e9dd771c70
gotoshota0116 <lgy.w30@gmail.com>
2025-02-06 16:58:28 +0900

    add:new,createルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'staticpages#top'
 
-  resources :posts, only: %i[index]
+  resources :posts, only: %i[index new create]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

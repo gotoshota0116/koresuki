05c01c4006229b42e144225dc8476079f15b3307
gotoshota0116 <lgy.w30@gmail.com>
2025-02-07 17:48:09 +0900

    style:フラッシュメッセージのデザイン修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,8 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
+      flash[:success] = t('defaults.flash_message.created', item: Post.model_name.human)
+      redirect_to posts_path
     else
       flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
       render :new, status: :unprocessable_entity
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
   def flash_class(key)
     case key.to_sym # key が文字列でも対応できるように to_sym で変換
-    when :notice then 'bg-green-500 text-white p-4 rounded-md'
-    when :alert  then 'bg-red-500 text-white p-4 rounded-md'
+    when :notice then "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md shadow-md mb-4"
+    when :alert  then "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-md shadow-md mb-4"
+    when :success then "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md shadow-md mb-4"
+    when :danger then "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-md shadow-md mb-4"
     else 'bg-blue-500 text-white p-4 rounded-md'
     end
   end

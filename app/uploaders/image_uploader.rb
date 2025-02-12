e10d152f5842f0332967dcb1293c91fbc52ae3db
gotoshota0116 <lgy.w30@gmail.com>
2025-02-12 18:19:02 +0900

    add:heic形式もアップロード可能に加筆

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick, MiniMagick, or Vips support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  # include CarrierWave::Vips
+include CarrierWave::MiniMagick
 
   if Rails.env.production?
     storage :fog # 本番環境ではS3に保存
@@ -10,41 +7,36 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :file # 開発・テスト環境ではローカルに保存
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  process :convert_heic_to_jpg, if: :heic?
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_allowlist
-    %w(jpg jpeg gif png)
+  def heic?(file)
+    file.extension.downcase == 'heic'
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg"
-  # end
+  def convert_heic_to_jpg
+    manipulate! do |img|
+      img.format('jpg')
+      img
+    end
+  end
+
+  # メソッドをオーバーライドして、HEIC形式の画像をJPG形式に変換する
+  def filename
+    if original_filename.present?
+      if file.extension.downcase == 'heic'
+        "#{File.basename(original_filename, '.*')}.jpg"
+      else
+        original_filename
+      end
+    end
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png heic)
+  end
+
 end

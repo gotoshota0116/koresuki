8ced342de213c97c44eff3fc90282c2f864fcfee
gotoshota0116 <lgy.w30@gmail.com>
2025-02-17 23:28:33 +0900

    add:ルーティング、profilesコントローラー、view作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
+  resource :profile, only: [:show, :edit, :update]
+
   get 'ogp/ogp.png', to: 'ogp_images#show', as: :ogp_image
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?

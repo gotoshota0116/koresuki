ca6933d35fca3c74003e0930f7adfa10b022820f
gotoshota0116 <lgy.w30@gmail.com>
2025-02-19 00:55:12 +0900

    add:i18n対応,ymlファイル追加実施

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
 	def create
 	  comment = current_user.comments.build(comment_params)
       if comment.save
-	  	flash[:notice] = t('.success')
+		flash[:notice] = t('defaults.flash_message.created', item: Comment.model_name.human)
         redirect_to post_path(comment.post)
       else
-	  	flash[:alert] = t('.failure')
+		flash[:alert] = t('defaults.flash_message.not_created', item: Comment.model_name.human)
         redirect_to post_path(comment.post)
       end
 	end
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
 	def destroy
 	  comment = current_user.comments.find(params[:id])
 	  comment.destroy!
-	  flash[:notice] = t('.success')
+	  flash[:notice] = t('defaults.flash_message.deleted', item: Comment.model_name.human)
 	  redirect_to post_path(comment.post)
 	end
   

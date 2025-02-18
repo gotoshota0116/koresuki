d40b8d1f3b13f1cb34d2c97b781fe0792ab7b205
gotoshota0116 <lgy.w30@gmail.com>
2025-02-18 20:39:48 +0900

    add:モデル関連付け実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :confirmable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 

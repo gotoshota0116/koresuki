e0f92692224b3d01629c18d9c0334d9e454781be
gotoshota0116 <lgy.w30@gmail.com>
2025-02-20 21:25:08 +0900

    add:モデル関連付け実施

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  # ポリモーフィック関連付けに変更
+  has_many :likes, as: :likeable, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   mount_uploader :image, ImageUploader

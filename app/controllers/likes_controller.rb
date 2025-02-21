05f48ba6bb22869d51e53ae5364777bdc22bb688
gotoshota0116 <lgy.w30@gmail.com>
2025-02-21 18:04:09 +0900

    refactor:リファクター実施、これからコメントにいいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_likeable, only: %i[create destroy]
+
   def create
-    @likeable = params[:likeable_type].constantize.find(params[:likeable_id])
-    pp 999999999999
-    pp @likeable
-    pp 9999999999
     current_user.like(@likeable)
+    # create.turbo_stream.erbで、like-button-#{@likeable.id}を更新する
   end
 
   def destroy
-    @likeable = params[:likeable_type].constantize.find(params[:likeable_id])
-    pp 8888888888
-    pp @likeable
-    pp 88888888888
     current_user.unlike(@likeable)
+    # destroy.turbo_stream.erbで、unlike-button-#{@likeable.id}を更新する
   end
 
-  # private
+  private
 
-  # def set_likeable
-  #   @likeable = params[:likeable_type].constantize.find(params[:likeable_id])
-  # end
+  def set_likeable
+    @likeable = params[:likeable_type].constantize.find(params[:likeable_id])
+  end
 end

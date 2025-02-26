f42dbe6562761c04018fb6b54b0e0a483bf9ef75
gotoshota0116 <lgy.w30@gmail.com>
2025-02-26 17:34:06 +0900

    add:PostImageアップローダー作成、モデルにマウント

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,6 @@
 class PostImage < ApplicationRecord
 	belongs_to :post
-	
+
+	mount_uploader :image, PostImageUploader
+
 end
---
+++ app/uploaders/post_image_uploader.rb
@@ -0,0 +1,34 @@
+class PostImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.production?
+    storage :fog # 本番環境ではS3に保存
+  else
+    storage :file # 開発・テスト環境ではローカルに保存
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_allowlist
+    %w[jpg jpeg gif png heic webp]
+  end
+
+  process resize_to_fit: [1000, 750]
+  process :convert_to_webp
+
+  private
+
+  def convert_to_webp
+    manipulate! do |img|
+      img.format('webp')
+      img
+    end
+  end
+
+  # 内部的に呼び出されるメソッド,filenameをオーバーライドしている
+ def filename
+    super.chomp(File.extname(super)) + '.webp' if original_filename.present?
+  end
+end

5cc03f31da3fce28e62188ef793e401d8b939712
gotoshota0116 <lgy.w30@gmail.com>
2025-02-28 02:00:35 +0900

    add:post_videoモデル、before_saveコールバック定義

--- app/models/post_video.rb
+++ app/models/post_video.rb
@@ -1,3 +1,11 @@
 class PostVideo < ApplicationRecord
   belongs_to :post
+
+  before_save :split_id_from_youtube_url
+
+
+  def split_id_from_youtube_url
+    # YoutubeならIDのみ抽出
+    youtube_url.split('/').last
+  end
 end

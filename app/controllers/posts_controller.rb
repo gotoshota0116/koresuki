4061be66c88cfa0c5f6ffdc89ade358f4145e8a6
gotoshota0116 <lgy.w30@gmail.com>
2025-03-03 12:03:25 +0900

    refactor:投稿一覧を最新の投稿から上から表示するように、orderする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.includes(:user, :likes)
+    @posts = Post.includes(:user, :likes).order(created_at: :desc)
   end
 
   def show

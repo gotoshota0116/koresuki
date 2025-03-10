612243386810101bae8739d4178f837e63c9134d
gotoshota0116 <lgy.w30@gmail.com>
2025-03-10 19:39:58 +0900

    style:lint修正実施

--- app/finders/posts_finder.rb
+++ app/finders/posts_finder.rb
@@ -1,26 +1,26 @@
 # 検索条件を受け取り、絞り込みを行うクラス
 class PostsFinder
-  def initialize(q)
-    @q = q #　検索条件　q [keyword: "入力値"]
+  def initialize(query)
+    @q = query # 　検索条件　query [keyword: "入力値"]
     @record = Post.all
   end
-  
+
   # 検索を実行する
   def search
     search_keyword
     @record
   end
-  
+
   private
-  
+
   # 検索キーワード(@q)を受け取り、該当レコードを取得して@recordにいれる
   def search_keyword
     return if @q.keyword.blank?
-    
+
     keyword = like_search_condition(@q.keyword)
-    @record = @record.where("title LIKE ? OR body LIKE ?", keyword, keyword)
+    @record = @record.where('title LIKE ? OR body LIKE ?', keyword, keyword)
   end
-  
+
   # like検索
   def like_search_condition(word)
     "%#{ActiveRecord::Base.sanitize_sql_like(word)}%"

646cf5789c162452701082fdea8584933b3307db
gotoshota0116 <lgy.w30@gmail.com>
2025-03-11 18:29:03 +0900

    style:lint修正実施

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,4 +1,1 @@
-if defined?(Bullet)
-	Bullet.add_safelist type: :unused_eager_loading, class_name: 'Notification', association: :visitor
-end
-  
+Bullet.add_safelist type: :unused_eager_loading, class_name: 'Notification', association: :visitor if defined?(Bullet)

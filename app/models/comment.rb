29f7bee57c06e30a7786647865b18d5e60a4c5c3
gotoshota0116 <lgy.w30@gmail.com>
2025-03-11 21:52:12 +0900

    fix:通知機能修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,19 +9,32 @@ class Comment < ApplicationRecord
 
   validates :body, presence: true, length: { maximum: 300 }
 
-  def create_notification(visitor, action)
-    # いいねユーザーとコメント作成者が同じ場合は通知を作成しない
-    return if visitor.id == user.id
 
-    Notification.create!(
-      # current_userが入る
-      visitor_id: visitor.id,
-      # コメントしたユーザー
-      visited_id: user.id,
-      # 通知の対象、コメントが入る
-      notifiable: self,
-      # いいねが入る
-      action: action
-    )
+  #　呼び出される時
+  # 1コメントに対していいね作成時 2コメント作成時（else)
+  def create_notification(visitor, action)
+    if action == :liked
+      # いいねした人とコメント作成者が同じ場合は通知しない
+      return if visitor.id == user.id
+      # 同じコメントに繰り返しいいねの通知を防ぐため、find_or_initialize_by
+      notification = Notification.find_or_initialize_by(
+        visitor_id: visitor.id  # current_user
+        visited_id: user.id, # コメント作成者
+        notifiable: self, # 通知元、like
+        action: action # liked
+      )
+      notification.save
+    else # コメント作成時
+      # コメントしたユーザーと投稿作成者が同じ場合は通知を作成しない
+      return if visitor.id == post.user.id
+      
+      Notification.create!(
+        visitor_id: visitor.id, # current_user
+        visited_id: post.user.id, # 投稿作成者
+        notifiable: self, # 通知元、comment
+        action: action # commented
+      )
+    end
   end
 end
+

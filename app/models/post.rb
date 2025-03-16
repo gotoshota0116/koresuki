2256bc5596f720068282caf86cf5dc7486d07fc1
gotoshota0116 <lgy.w30@gmail.com>
2025-03-16 23:52:05 +0900

    add:モデル関連付け実施

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :post_images, allow_destroy: true, reject_if: :all_blank
   has_many :post_videos, dependent: :destroy
   accepts_nested_attributes_for :post_videos, allow_destroy: true, reject_if: :all_blank
+  has_many :post_categories, dependent: :destroy
+  has_many :categories, through: :post_categories
 
   mount_uploader :image, ImageUploader
 

e2cb1b5c5722d52c7e918b5ddae20d60c8c0ef65
gotoshota0116 <lgy.w30@gmail.com>
2025-03-17 13:00:47 +0900

    add:カテゴリ投稿view編集、i18n実施

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# ユーザー投稿の作成
+# 10.times do
+#   User.create!(name: Faker::Name.name,
+#                email: Faker::Internet.unique.email,
+#                password: 'password',
+#                password_confirmation: 'password')
+# end
 
-10.times do
-  User.create!(name: Faker::Name.name,
-               email: Faker::Internet.unique.email,
-               password: 'password',
-               password_confirmation: 'password')
-end
+# user_ids = User.pluck(:id)
 
-user_ids = User.pluck(:id)
+# 20.times do |index|
+#   user = User.find(user_ids.sample)
+#   user.posts.create!(title: "タイトル#{index}", body: "本文#{index}")
+# end
 
-20.times do |index|
-  user = User.find(user_ids.sample)
-  user.posts.create!(title: "タイトル#{index}", body: "本文#{index}")
+# カテゴリの初期値の作成
+categories = [
+  "趣味・娯楽",
+  "映画・ドラマ・アニメ",
+  "本・漫画",
+  "音楽",
+  "ゲーム",
+  "お笑い",
+  "スポーツ",
+  "動画・配信",
+  "アート・デザイン",
+  "ファッション・衣服",
+  "健康・美容",
+  "ライフスタイル・暮らし",
+  "食べ物・飲み物",
+  "モノ・商品",
+  "自然・アウトドア",
+  "場所・旅行",
+  "動物・ペット",
+  "歴史・文化・伝統",
+  "人・キャラクター",
+  "その他"
+]
+
+categories.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
 end

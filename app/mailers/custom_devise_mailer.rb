b23594b243da8c2f3a713234b47dc1efe0c0a235
gotoshota0116 <lgy.w30@gmail.com>
2025-03-19 19:47:55 +0900

    refactor:サービス名変更に伴う、メール送信元の変更

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,5 +1,5 @@
 class CustomDeviseMailer < Devise::Mailer
-  default from: 'koresuki.info@gmail.com' # 送信元を変更
+  default from: 'sharesuki.info@gmail.com' # 送信元を変更
 
   def confirmation_instructions(record, token, opts = {})
     opts[:subject] = '【シェアスキ！】メールアドレスの確認' # 件名をカスタマイズ

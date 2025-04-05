dccbf37c429d8f2191fdd7d17edf99eff1e1f0f0
gotoshota0116 <lgy.w30@gmail.com>
2025-04-06 02:19:03 +0900

    add:ルーティング、userモデルに設定追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy

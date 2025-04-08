cbb38e27cc47fc0b16e5108e7137834d8f24de6d
gotoshota0116 <lgy.w30@gmail.com>
2025-04-08 18:37:49 +0900

    add:factorybotの設定追加、nameバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :uid, uniqueness: { scope: :provider }, allow_nil: true
 
   def own?(object)

aaccfaeb9985a1283ef36836681e231cc49233f9
gotoshota0116 <lgy.w30@gmail.com>
2025-04-08 23:19:32 +0900

    add:focut: trueが使用できるように設定追加
    add:userモデルのバリデーションテスト記載完了

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+RSpec.describe User do
+
+	describe "バリデーション成功" do
+		context "name,email,password,password_confirmationを正しく入力した場合" do
+	  	it "ユーザーが作成されること" do
+        user = build(:user)
+        expect(user).to be_valid
+        expect(user.errors).to be_empty
+	  	end
+		end
+	end
+
+  describe "バリデーション失敗" do
+    context "nameが空白の場合" do
+      it "バリデーションエラーが発生し、エラーメッセージが表示されること" do
+        user = build(:user, name: "a" * 256)
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("は255文字以内で入力してください")
+      end
+    end
+    context "nameが255文字を超える場合" do
+      it "バリデーションエラーが発生し、エラーメッセージが表示されること" do
+        user = build(:user, name: "")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("を入力してください")
+      end
+    end
+
+    context "emailが空白の場合" do
+      it "バリデーションエラーが発生し、エラーメッセージが表示されること" do
+        user = build(:user, email: "")
+        expect(user).to be_invalid
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+    end
+
+    context "passwordが空白の場合" do
+      it "バリデーションエラーが発生し、エラーメッセージが表示されること" do
+        user = build(:user, password: "")
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+    end
+
+    context "password_confirmationが空白の場合" do
+      it "バリデーションエラーが発生し、エラーメッセージが表示されること" do
+        user = build(:user, password_confirmation: "")
+        expect(user).to be_invalid
+        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      end
+    end
+
+    context "providerとuidが重複した場合" do
+      it "バリデーションが発生し、エラーメッセージが表示されること" do
+        user1 = create(:user, confirmed_at: Time.current, provider: "google_auth2", uid: "12345")
+        user2 = build(:user, provider: "google_auth2", uid: "12345")
+        expect(user2).to be_invalid
+        expect(user2.errors[:uid]).to include("はすでに存在します")
+      end
+    end
+  end
+
+end

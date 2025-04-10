4c57f93eb6ad7b815b32e31d2f2c00ad94e848a4
gotoshota0116 <lgy.w30@gmail.com>
2025-04-10 19:09:16 +0900

    add:post_video（Youtube)動画ファクトリー定義
    add:Youtube動画投稿テスト追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :post do
+    association :user
     title { 'test_title' }
     body { 'test_body' }
-    # association :user
 
     after(:build) do |post|
       post.categories << build(:category)
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,4 +14,33 @@ RSpec.describe Post do
   it { is_expected.to have_many(:categories).through(:post_categories) }
   it { is_expected.to have_many(:bookmarks).dependent(:destroy) }
 
+	describe 'バリデーション成功' do
+    context 'title,body,categoryを正しく入力した場合' do
+      it '投稿が作成されること' do
+        post = build(:post)
+        expect(post).to be_valid
+        expect(post.errors).to be_empty
+      end
+    end
+
+		# context 'サブ画像を入力した場合', focus: true  do
+    #   it '投稿が作成されること' do
+    #     post = build(:post)
+		# 		post_image = build(:post_image, post: post)
+		# 		post.post_images << post_image
+    #     expect(post).to be_valid
+    #     expect(post.errors).to be_empty
+    #   end
+    # end
+
+		context 'Youtube動画、説明文を入力した場合', focus: true  do
+      it '投稿が作成されること' do
+        post = build(:post)
+				post_video = build(:post_video)
+				post.post_videos << post_video
+        expect(post).to be_valid
+        expect(post.errors).to be_empty
+      end
+    end
+  end
 end

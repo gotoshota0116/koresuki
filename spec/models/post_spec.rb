b80f96d754ce6e078940d9edcca17da5541d9506
gotoshota0116 <lgy.w30@gmail.com>
2025-04-11 20:33:09 +0900

    refactor:不要な関連付けを削除

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Post do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:comments).dependent(:destroy) }
   it { is_expected.to have_many(:likes).dependent(:destroy) }
-  it { is_expected.to have_many(:liked_users).through(:likes).source(:user) }
   it { is_expected.to have_many(:notifications).dependent(:destroy) }
   it { is_expected.to have_many(:post_images).dependent(:destroy) }
   it { is_expected.to accept_nested_attributes_for(:post_images).allow_destroy(true) }

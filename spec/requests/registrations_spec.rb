d86d0913248808f6b7864651ceeae15acc15e271
gotoshota0116 <lgy.w30@gmail.com>
2025-04-12 14:48:01 +0900

    add:registrations（会員登録）リクエストスペック作成

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Registrations", type: :request do
+	let(:user) { create(:user) }
+
+	before { sign_in user }
+  
+	# オーバーライドしたregistrationsコントローラーのテスト
+	describe "ユーザー情報の編集" do
+		context 'パスワード入力なしでプロフィールを更新した場合' do
+	    it '更新され、プロフィール画面に遷移する' do
+         patch user_registration_path, params: {
+           user: {
+         	name: "新しい名前",
+         	email: user.email,
+         	password: ""
+           }
+				 }
+        expect(response).to redirect_to profile_path
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include("新しい名前")
+			end
+	  end
+	end
+end

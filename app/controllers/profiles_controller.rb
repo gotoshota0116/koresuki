f601e67a8c8065eb6c754547976a6f62bd0b8f9e
gotoshota0116 <lgy.w30@gmail.com>
2025-04-15 23:41:52 +0900

    style:Lint修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
-    @posts  = current_user.posts.includes(:post_videos).order(created_at: :desc)
+    @posts = current_user.posts.includes(:post_videos).order(created_at: :desc)
     @bookmark_posts = current_user.bookmark_posts.includes(:user, :post_videos).order(created_at: :desc)
     @current_user_likes = current_user.present? ? current_user.likes.where(likeable_type: 'Post').index_by(&:likeable_id) : {}
     @current_user_bookmarks = current_user.present? ? current_user.bookmarks.index_by(&:post_id) : {}

bcb9a2414748af202f8c95faf09821d4fb35bc7b
gotoshota0116 <lgy.w30@gmail.com>
2025-05-13 23:26:52 +0900

    add:オートコンプリートコントローラー登録、未ログインでもオートコンプリート可能に

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show search]
   before_action :set_post, only: %i[edit update destroy]
 
   def index
